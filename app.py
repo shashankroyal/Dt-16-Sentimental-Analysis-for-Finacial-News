8c423c5d44a53f4e7f3e6cbbfb843c9c6c8e62fe
Shashank <shashankroyal.2004@gmail.com>
2025-02-07 11:00:15 +0530

    hello

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/predict")
+def predict():
+    return render_template("predict.html")
 if __name__=="__main__":
-    app.run()
+    app.run(host="0.0.0.0",port=5050)
